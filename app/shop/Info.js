8cebc568fe8687369d6c3e1e1fead704ea3e4f12
tangpingqun <tangpingqun123@163.com>
2018-05-29 10:21:48 +0800

    tpq

--- app/shop/Info.js
+++ app/shop/Info.js
@@ -3,6 +3,7 @@ import {
     Platform,
     StyleSheet,
     View, Switch, FlatList, ImageBackground, ScrollView,
+    TextInput,FixedTextInput
 } from 'react-native';
 import { Text, Thumbnail, Icon, Item, Input, Button } from 'native-base';
 import Color from "../common/Color";
@@ -14,7 +15,7 @@ export default class Info extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            zt:false
         };
     }
 
@@ -88,8 +89,85 @@ export default class Info extends Component {
     render() {
         return(
             <View style={{flex:10, padding:5}}>
-                <Button onPress={()=>this.getImg()}><Text>图片上传</Text></Button>
-                <Button onPress={()=>this._showAreaPicker()}><Text>地区选择</Text></Button>
+                {/*<Button onPress={()=>this.getImg()}><Text>图片上传</Text></Button>*/}
+                {/*<Button onPress={()=>this._showAreaPicker()}><Text>地区选择</Text></Button>*/}
+                <View style={{flex:1, backgroundColor: 'white',flexDirection:'row'}}>
+                    <ScrollView style={{flex:1}}>
+                        <Text style={{lineHeight:100,textAlign:'center'}}>门店基础信息</Text>
+                        <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>门店头图：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <Thumbnail style={{marginLeft:20,width:120,height:80}} visible={false} square source={{uri: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1760172527,1473711532&fm=27&gp=0.jpg'}} />
+                                <View style={{marginRight:20}}>
+                                    <Button light onPress={()=>this.getImg()}><Text>上传新图</Text></Button>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={{flexDirection:'row',justifyContent:'space-between',height:60}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>门店名称：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <TextInput style={styles.textInput} placeholder="请输入门店名称" />
+                            </View>
+                        </View>
+                        <View style={{flexDirection:'row',justifyContent:'space-between',height:60}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>所在地：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <View><Button light onPress={()=>this._showAreaPicker()}><Text>地区选择</Text></Button></View>
+                            </View>
+                        </View>
+                        <View style={{flexDirection:'row',justifyContent:'space-between',height:60}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>详细地址：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <TextInput style={styles.textInput} value="朝阳3路234号景山大厦6栋2314号" />
+                            </View>
+                        </View>
+                        <View style={{flexDirection:'row',justifyContent:'space-between',height:60}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>联系人：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <TextInput style={styles.textInput} value="王先生" />
+                            </View>
+                        </View>
+                        <View style={{flexDirection:'row',justifyContent:'space-between',height:60}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>联系电话：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <TextInput style={styles.textInput} value="13564333333" />
+                            </View>
+                        </View>
+                        <View style={{flexDirection:'row',justifyContent:'space-between',height:60}}>
+                            <View style={{flex:1,justifyContent:'center'}}>
+                                <Text style={{textAlign:'center'}}>办公电话：</Text>
+                            </View>
+                            <View style={{flex:3,flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                                <TextInput style={styles.textInput} value="010-56326221" />
+                            </View>
+                        </View>
+                        <View style={{height:80,justifyContent:'center',alignItems:'center'}}>
+                            <View>
+                                <Button style={{flexDirection:'row',backgroundColor:'#16b8be',width:120,justifyContent:'center'}}><Text>保存信息</Text></Button>
+                            </View>
+                        </View>
+                    </ScrollView>
+                    <View style={{width:400,borderLeftWidth:1,borderColor:'#cad3df'}}>
+                       <TextInput style={styles.intro} underlineColorAndroid='transparent' />
+                        <View style={{height:80,justifyContent:'center',alignItems:'center'}}>
+                            <View>
+                                <Button style={{flexDirection:'row',backgroundColor:'#16b8be',width:120,justifyContent:'center'}}><Text>保存信息</Text></Button>
+                            </View>
+                        </View>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -101,5 +179,7 @@ const styles = StyleSheet.create({
     text: { margin: 6, textAlign:'center' },
     row: { flexDirection: 'row', backgroundColor: 'white'  },
     btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
-    btnText: { textAlign: 'center', color: '#fff' }
+    btnText: { textAlign: 'center', color: '#fff' },
+    textInput:{flex:1,marginRight:50},
+    intro:{ width:350,height:400,marginTop:20,marginLeft:25,borderWidth:1,borderColor:'#dedede',textAlignVertical: 'top'}
 });
