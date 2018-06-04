dedb4aa615b350c67ead0b9a114343ecf99bbf6a
tangpingqun <tangpingqun123@163.com>
2018-06-04 16:29:09 +0800

    app

--- app/goods/Clear.js
+++ app/goods/Clear.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import {
     Platform,
     StyleSheet,
-    View, Switch, FlatList, ImageBackground,
+    View, Switch, FlatList, ImageBackground,Modal,TouchableOpacity
 } from 'react-native';
-import { Text, Thumbnail, Icon, Item, Input, Button, Card, CardItem, Body } from 'native-base';
+import { Text, Thumbnail, Icon, Item, Input, Button, Card, CardItem, Body,ListItem,Radio,Left,Right,Content } from 'native-base';
 import Color from "../common/Color";
 import MyLeft from "../common/Left";
 import Bottom from "../common/Bottom";
@@ -15,21 +15,22 @@ export default class Index extends MyLeft {
         super(props);
         Object.assign(this.state, {
             tables: [
-                {name: 'A01', num: 5, price: 1125},
-                {name: 'A02', num: 6, price: 1253},
-                {name: 'A03', num: 3, price: 1215},
-                {name: 'A04', num: 4, price: 12215},
-                {name: 'A05', num: 7, price: 1215},
-                {name: 'A06', num: 12, price: 2125},
-                {name: 'A06', num: 12, price: 2125},
-                {name: 'A06', num: 12, price: 2125},
-                {name: 'A06', num: 12, price: 2125},
-                {name: 'A06', num: 12, price: 2125},
-                {name: 'A06', num: 12, price: 2125},
-                {name: 'A06', num: 12, price: 2125},
+                {name: 'A01', num: 5, price: 1125,islower:true},
+                {name: 'A02', num: 6, price: 1253,islower:false},
+                {name: 'A03', num: 3, price: 1215,islower:false},
+                {name: 'A04', num: 4, price: 12215,islower:false},
+                {name: 'A05', num: 7, price: 1215,islower:false},
+                {name: 'A06', num: 12, price: 2125,islower:false},
+                {name: 'A06', num: 12, price: 2125,islower:false},
+                {name: 'A06', num: 0, price: 2125,islower:false},
+                {name: 'A06', num: 12, price: 2125,islower:false},
+                {name: 'A06', num: 12, price: 2125,islower:false},
+                {name: 'A06', num: 0, price: 2125,islower:false},
+                {name: 'A06', num: 12, price: 2125,islower:false},
             ],
             type: 1,
             nowNav: 2,
+            zt:false
             /*leftClassify: [
                 {name: '全部', type: 1},
                 {name: '未沽清', type: 2},
@@ -74,16 +75,91 @@ export default class Index extends MyLeft {
                     <FlatList data={this.state.tables} columnWrapperStyle={{justifyContent:'center', flexWrap:'wrap'}}
                               renderItem={({item})=>this._item(item)} numColumns={8} keyExtractor={({v, k})=>k}/>
                 </View>
+
+                <Modal animationType={'fade'} visible={this.state.zt} onRequestClose={()=>this.setState({zt: false})} transparent={true}>
+                    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                        <View style={{backgroundColor:'#1a1a1a',borderRadius:8,padding:30}}>
+
+                            <View style={{flexDirection:'row',alignItems:'center',height:50}}>
+                                <Text style={{color:'#fff'}}>名称:</Text>
+                                <Text style={{color:'#fff',paddingLeft:20}}>牛油果烟熏三文鱼</Text>
+                            </View>
+
+                            <View style={{flexDirection:'row',alignItems:'center',height:50}}>
+                                <Text style={{color:'#fff'}}>份数:</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40,marginLeft:20}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='留空则不限量供应'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',alignItems:'center',height:50}}>
+                                <Text style={{color:'#fff'}}>类型:</Text>
+                                <View style={{flexDirection:'row',alignItems:'center',marginLeft:20}}>
+                                    <Radio color={"#fff"} selectedColor={"#fff"} selected={false}/>
+                                    <Text style={{color:'#fff'}}>按天清零</Text>
+                                    <Radio color={"#fff"} style={{marginLeft:20}} selectedColor={"#fff"} selected={true}/>
+                                    <Text style={{color:'#fff'}}>按总数量</Text>
+
+                                </View>
+
+                            </View>
+
+
+                            <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',height:60}}>
+                                <View style={{marginRight:30}}>
+                                    <Button style={{height:40,width:120,backgroundColor:'#999999',borderRadius:40,justifyContent:'center'}}>
+                                        <Text style={{color:'#fff'}}>下架商品</Text>
+                                    </Button>
+                                </View>
+
+                                <View>
+                                    <Button style={{height:40,width:120,backgroundColor:'#8dc23c',borderRadius:40,justifyContent:'center'}} onPress={()=>{this.setState({zt:false})}}>
+                                        <Text style={{color:'#fff'}}>确定</Text>
+                                    </Button>
+                                </View>
+
+                            </View>
+
+                        </View>
+                    </View>
+                </Modal>
+
             </View>
         );
     }
 
     _item(item) {
-        return(
-            <View style={{width:(WIDTH-150)/8, height: 100, elevation: 2, backgroundColor: 'white', margin:5, padding:5}}>
-                <Text style={{height:70, fontSize:14}}>安居为阿斯蒂阿萨啊</Text>
-                <Text style={{height:20, lineHeight:20}}>&yen;128</Text>
-            </View>
-        )
+        if(item.num == 0){
+            return(
+                <View style={{width:(WIDTH-150)/8, height: 100, elevation: 2, backgroundColor: 'white', margin:5, padding:5}}>
+                    <Text style={{height:70, fontSize:14}}>安居为阿斯蒂阿萨啊</Text>
+                    <Text style={{height:20, lineHeight:20}}>&yen;{item.price}</Text>
+                    <ImageBackground style={{width:30,height:30,position:'absolute',right:0,bottom:0}} source={require('../bg/redCoder.png')}>
+                        <Text style={{color:'#fff',textAlign:'right',paddingRight:2,lineHeight:40,fontSize:15}}>{item.num}</Text>
+                    </ImageBackground>
+                </View>
+            )
+        }
+        else if(item.islower){
+            return(
+                <ImageBackground style={{width:(WIDTH-150)/8, height: 100, elevation: 2, backgroundColor: 'white', margin:5, padding:5}} source={require('../bg/Lowerframe.png')}>
+                    <Text style={{height:70, fontSize:14}}>安居为阿斯蒂阿萨啊</Text>
+                    <Text style={{height:20, lineHeight:20}}>&yen;{item.price}</Text>
+                </ImageBackground>
+            )
+        }
+        else{
+            return(
+                <TouchableOpacity onPress={()=>{this.setState({zt:true})}}>
+                    <ImageBackground style={{width:(WIDTH-150)/8, height: 100, elevation: 2, backgroundColor: 'white', margin:5, padding:5}}>
+                        <Text style={{height:70, fontSize:14}}>安居为阿斯蒂阿萨啊</Text>
+                        <Text style={{height:20, lineHeight:20}}>&yen;{item.price}</Text>
+                        <ImageBackground style={{width:30,height:30,position:'absolute',right:0,bottom:0}} source={require('../bg/Coder.png')}>
+                            <Text style={{color:'#fff',textAlign:'right',paddingRight:2,lineHeight:40,fontSize:15}}>{item.num}</Text>
+                        </ImageBackground>
+                    </ImageBackground>
+                </TouchableOpacity>
+            )
+        }
     }
 }
--- app/member/Index.js
+++ app/member/Index.js
@@ -4,7 +4,7 @@ import {
     StyleSheet,
     View, Switch, FlatList, ImageBackground,Modal
 } from 'react-native';
-import { Text, Thumbnail, Icon, Item, Input, Button, Card, CardItem, Body } from 'native-base';
+import { Text, Thumbnail, Icon, Item, Input, Button, Card, CardItem, Body,Form,Picker,Textarea } from 'native-base';
 import Color from "../common/Color";
 import MyLeft from "../common/Left";
 import Bottom from "../common/Bottom";
@@ -31,13 +31,19 @@ export default class Index extends MyLeft {
             nowNav: 5,
             zt:false,
             zt1:false,
-            zt2:false
+            zt2:false,
+            selected2:undefined
+        });
+    }
+    onValueChange2(value) {
+        this.setState({
+            selected2: value
         });
     }
 
     _init() {
         return (
-            <View style={{width: WIDTH*14/15, height: HEIGHT-20}}>
+            <View style={{width: WIDTH*14/15, height: HEIGHT-70}}>
                 <Top com={
                     <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
                         <View style={{height: GAO, width: 320, borderRadius: 5, flexDirection: 'row'}}/>
@@ -46,7 +52,7 @@ export default class Index extends MyLeft {
                             <Icon name={'search'} type={'EvilIcons'} style={{fontSize: 35}}/>
                         </Item>
                         <View>
-                            <Button iconLeft style={{backgroundColor: Color.tableIndex.photoBg}} small>
+                            <Button iconLeft style={{backgroundColor: Color.tableIndex.photoBg}} small onPress={()=>{this.setState({zt2:true})}}>
                                 <Text>开卡</Text>
                             </Button>
                         </View>
@@ -71,32 +77,229 @@ export default class Index extends MyLeft {
                             </View>
 
                             <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
-                                <Text style={{paddingLeft:30}}>会员名称：</Text>
-                                <Text style={{paddingLeft:30}}>acc</Text>
+                                <Text style={{paddingLeft:30,fontSize:14}}>会员名称：</Text>
+                                <Text style={{fontSize:14}}>acc</Text>
                             </View>
 
                             <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
-                                <Text style={{paddingLeft:30}}>会员号码：</Text>
-                                <Text style={{paddingLeft:30}}>1852356699</Text>
+                                <Text style={{paddingLeft:30,fontSize:14}}>会员号码：</Text>
+                                <Text style={{fontSize:14}}>1852356699</Text>
                             </View>
 
                             <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
-                                <Text style={{paddingLeft:30}}>余额：</Text>
-                                <Text style={{paddingLeft:30,fontSize:26,color:'#8dc23c'}}>0.00</Text>
+                                <Text style={{paddingLeft:30,fontSize:14}}>余额：</Text>
+                                <Text style={{fontSize:26,color:'#8dc23c'}}>0.00</Text>
                             </View>
 
                             <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
-                                <Text style={{paddingLeft:30}}>充值金额：</Text>
-                                <Item bordered style={{width:150}}>
-                                    <Input bordered style={{width:150}} placeholder='请输入充值金额'/>
+                                <Text style={{paddingLeft:30,fontSize:14}}>充值金额：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入充值金额'/>
                                 </Item>
                             </View>
 
                             <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
-                                <Text style={{paddingLeft:30}}>在线支付：</Text>
-                                <View><Button><Text>在线支付</Text></Button></View>
-                                <View><Button><Text>现金</Text></Button></View>
+                                <Text style={{paddingLeft:30,fontSize:14}}>在线支付：</Text>
+                                <View style={{marginRight:20}}><Button style={{backgroundColor:'#8dc23c',height:40}}><Text style={{fontSize:14}}>在线支付</Text></Button></View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>现金</Text></Button>
+                                </View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>银行卡</Text></Button>
+                                </View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>支付宝口碑</Text></Button>
+                                </View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>信用社融合付</Text></Button>
+                                </View>
                             </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>赠送金额：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input  style={{width:180,fontSize:14}} placeholder='请输入赠送金额'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>发票金额：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input  style={{width:180,fontSize:14}} placeholder='请输入发票金额'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',marginBottom:20,height:60}}>
+                                <View style={{marginRight:50}}>
+                                    <Button style={{backgroundColor:'#fa7159',height:35}}><Text style={{color:'#fff',paddingLeft:30,paddingRight:30,fontSize:14}}>充值</Text></Button>
+                                </View>
+                                <View>
+                                    <Button style={{backgroundColor:'#b3b3b3',height:35}}><Text style={{color:'#fff',paddingLeft:30,paddingRight:30,fontSize:14}} onPress={()=>{this.setState({zt:false})}}>取消</Text></Button>
+                                </View>
+                            </View>
+
+                        </View>
+
+                    </View>
+                </Modal>
+
+                <Modal animationType={'fade'} visible={this.state.zt1} onRequestClose={()=>this.setState({zt1: false})} transparent={true}>
+                    <View style={{flex:1,backgroundColor:'rgba(0,0,0,0.5)',justifyContent:'center',alignItems:'center'}}>
+
+                        <View style={{backgroundColor:'#fff'}}>
+
+                            <View style={{backgroundColor:'#8dc23c',height:40}}>
+                                <Text style={{color:'#fff',paddingLeft:30,lineHeight:40,paddingRight:30}}>赠送优惠券</Text>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>会员名称：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input style={{width:180,fontSize:14}} placeholder='请输入会员名称'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>选择赠送：</Text>
+                                <Form style={{width:180,borderWidth:2,borderColor:'#dedede',height:40,borderRadius:5}}>
+                                    <Picker
+                                        iosIcon={<Icon name="ios-arrow-down-outline" />}
+                                        placeholder="Select your SIM"
+                                        placeholderStyle={{ color: "#bfc6ea" }}
+                                        placeholderIconColor="#007aff"
+                                        style={{ width: 180,height:40}}
+                                        selectedValue={this.state.selected2}
+                                        onValueChange={this.onValueChange2.bind(this)}
+                                    >
+                                        <Picker.Item label="Wallet" value="key0" />
+                                        <Picker.Item label="ATM Card" value="key1" />
+                                        <Picker.Item label="Debit Card" value="key2" />
+                                        <Picker.Item label="Credit Card" value="key3" />
+                                        <Picker.Item label="Net Banking" value="key4" />
+                                    </Picker>
+                                </Form>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>赠送数量：</Text>
+                                <Item rounded style={{width:100,height:40,borderRadius:5}}>
+                                    <Input rounded style={{width:100,textAlign:'center',fontSize:14}} value='1'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:120,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>赠送理由：</Text>
+                                <Textarea style={{width:300,height:120,borderRadius:5,fontSize:14}} bordered placeholder="请输入赠送理由" />
+                            </View>
+
+                            <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',height:60}}>
+                                <View style={{marginRight:50}}>
+                                    <Button  style={{backgroundColor:'#fa7159',height:35}}><Text style={{color:'#fff',paddingLeft:30,paddingRight:30,fontSize:14}}>充值</Text></Button>
+                                </View>
+                                <View>
+                                    <Button style={{backgroundColor:'#b3b3b3',height:35}}><Text style={{color:'#fff',paddingLeft:30,paddingRight:30,fontSize:14}} onPress={()=>{this.setState({zt1:false})}}>取消</Text></Button>
+                                </View>
+                            </View>
+
+                        </View>
+
+                    </View>
+                </Modal>
+
+                <Modal animationType={'fade'} visible={this.state.zt2} onRequestClose={()=>this.setState({zt2: false})} transparent={true}>
+                    <View style={{flex:1,backgroundColor:'rgba(0,0,0,0.5)',justifyContent:'center',alignItems:'center'}}>
+
+                        <View style={{backgroundColor:'#fff'}}>
+
+                            <View style={{backgroundColor:'#8dc23c',height:40}}>
+                                <Text style={{color:'#fff',paddingLeft:30,lineHeight:40,paddingRight:30,fontSize:14}}>会员充值</Text>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>会员开卡：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入会员名称'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>会员号码：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input type="number" bordered style={{width:180,fontSize:14}} placeholder='请输入手机号码'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>卡&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;号：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入卡号'/>
+                                </Item>
+                                <View style={{marginLeft:20}}>
+                                    <Button style={{height:40,backgroundColor:'#8dc23c'}}><Text style={{fontSize:14}}>手机号</Text></Button>
+                                </View>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>身份证号：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入身份证号'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>生&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;日：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='格式 2018-03-02'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>在线支付：</Text>
+                                <View style={{marginRight:20}}><Button style={{backgroundColor:'#8dc23c',height:40}}><Text>在线支付</Text></Button></View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>现金</Text></Button>
+                                </View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>银行卡</Text></Button>
+                                </View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>支付宝口碑</Text></Button>
+                                </View>
+                                <View style={{marginRight:20}}>
+                                    <Button style={{backgroundColor:'#fff',borderColor:'#dedede',height:40,borderRadius:5}}><Text style={{color:'#333',fontSize:14}}>信用社融合付</Text></Button>
+                                </View>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>充值金额：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入充值金额'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>赠送金额：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入赠送金额'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',marginRight:30,height:60,alignItems:'center'}}>
+                                <Text style={{paddingLeft:30,fontSize:14}}>发票金额：</Text>
+                                <Item rounded style={{width:180,borderRadius:5,height:40}}>
+                                    <Input bordered style={{width:180,fontSize:14}} placeholder='请输入发票金额'/>
+                                </Item>
+                            </View>
+
+                            <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',marginBottom:10,height:60}}>
+                                <View style={{marginRight:50}}>
+                                    <Button style={{backgroundColor:'#fa7159',height:35}}><Text style={{color:'#fff',paddingLeft:30,paddingRight:30,fontSize:14}}>确定</Text></Button>
+                                </View>
+                                <View>
+                                    <Button style={{backgroundColor:'#b3b3b3',height:35}}><Text style={{color:'#fff',paddingLeft:30,paddingRight:30,fontSize:14}} onPress={()=>{this.setState({zt2:false})}}>取消</Text></Button>
+                                </View>
+                            </View>
+
                         </View>
 
                     </View>
@@ -126,7 +329,7 @@ export default class Index extends MyLeft {
                     </View>
                 </View>
                 <View style={{backgroundColor:'#A39104', flexDirection:'row', justifyContent:'flex-end',height:30}}>
-                    <Text style={{color: 'white', width: 80,textAlign:'center',height: 30, lineHeight:30, backgroundColor:'#019D86'}}>送券</Text>
+                    <Text style={{color: 'white', width: 80,textAlign:'center',height: 30, lineHeight:30, backgroundColor:'#019D86'}} onPress={()=>{this.setState({zt1:true})}}>送券</Text>
                     <Text style={{color: 'white', width: 80,textAlign:'center',height: 30, lineHeight:30}} onPress={()=>{this.setState({zt:true})}}>充值</Text>
                 </View>
             </View>
